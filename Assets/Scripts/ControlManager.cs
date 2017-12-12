3216e4de112e78d6b5fc5aa8baaeb54c3cbd8ee7
Barudy Diego <barudy.diego@gmail.com>
2017-12-12 03:02:58 +0100

    commit click ready

--- Assets/Scripts/ControlManager.cs
+++ Assets/Scripts/ControlManager.cs
@@ -24,7 +24,7 @@ public class ControlManager : Game
     {
         	
     }
-
+    
     #endregion
 
     #region Tools Debug and Utility
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -35,8 +35,24 @@ public class Game : MonoBehaviour
     void StartGame (){
         m_asking_list= AskingContainer.XmlToList(pathXmlAsking);
         m_npc_list = NpcContainer.XmlToList(pathXmlNpc);
-        m_npc_zone.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Npc/"+ m_npc_list[0].image+".png");
+        m_npc_zone.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Npc/"+ m_npc_list[0].image);
         m_ask_zone.GetComponent<Text>().text = m_asking_list[0].ask;
+    }
+    public void ClickYes() {
+        int rnd_npc= Random.Range(0, m_npc_list.Count);
+        int rnd_ask= Random.Range(0, m_asking_list.Count);
+        Debug.Log(rnd_npc+" = "+ "Assets/Sprites/Npc/" + m_npc_list[rnd_npc].image +" " + rnd_ask);
+        m_npc_zone.GetComponent<Image>().sprite= AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Npc/" + m_npc_list[rnd_npc].image );
+        m_ask_zone.GetComponent<Text>().text = m_asking_list[rnd_ask].ask;
+    }
+   public  void ClickNo()
+    {
+        int rnd_npc = Random.Range(0, m_npc_list.Count);
+        int rnd_ask = Random.Range(0, m_asking_list.Count);
+        Debug.Log(rnd_npc + " = " + "Assets/Sprites/Npc/" + m_npc_list[rnd_npc].image + " " + rnd_ask);
+        m_npc_zone.GetComponent<Image>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Sprites/Npc/" + m_npc_list[rnd_npc].image);
+        m_ask_zone.GetComponent<Text>().text = m_asking_list[rnd_ask].ask;
+
     }
     #endregion
 
@@ -45,5 +61,10 @@ public class Game : MonoBehaviour
     private RectTransform m_npc_zone;
     [SerializeField]
     private RectTransform m_ask_zone;
+    [SerializeField]
+    private Button m_yes;
+    [SerializeField]
+    private Button m_no;
     #endregion
+
 }
